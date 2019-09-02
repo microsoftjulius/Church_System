34b27cf3c8499c3239eb4b69264d6e6d9f3a078c
microsoftjulius <julisema4@gmail.com>
2019-09-02 04:20:44 -0400

    Church Controllers Worked On

--- app/Http/Controllers/ChurchesController.php
+++ app/Http/Controllers/ChurchesController.php
@@ -7,33 +7,43 @@ use Illuminate\Http\Request;
 
 class ChurchesController extends Controller
 {
-    //Redirect to a page showing all churches
+    //Redirect to a page showing all churches and creating all churches
     public function index()
     {
         return view('after_login.home');
     }
 
     //Create a new Church
-    public function create()
+    public function create(Request $request)
     {
-        //
+        churches::create(array(
+            'church_name' => $request->church_name
+        ));
+        return Redirect()->back()->withErrors('Church Created Successfully');
     }
 
     //Edit the Church information
-    public function edit(churches $churches)
+    public function edit(Request $request)
     {
-        //
+        churches::where('id',$request->church_id)->update(array(
+            'background_color'=>$request->background_color
+        ));
+        return Redirect()->back()->withErrors('Background Color Edited Successfully');
     }
 
     //Update church as Active
     public function update(Request $request, churches $churches)
     {
-        //
+        churches::where('id',$request->church_id)->update(array(
+            'status'=>'active'
+        ));
     }
 
     //Mark Church as Deleted
-    public function destroy(churches $churches)
+    public function destroy(Request $request)
     {
-        //
+        churches::where('id',$request->church_id)->update(array(
+            'status'=>'deleted'
+        ));
     }
 }
