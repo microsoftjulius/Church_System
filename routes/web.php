e3ba598b23aeb50641738a16f3da0a4c8f429fdf
ociba <ocibajames@gmail.com>
2019-09-06 12:04:28 +0300

    removed form from create

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/church','ChurchesController@index_showall');
 
 Route::get('/create-user',function() { return view('after_login.create-users');});
 
+Route::get('/add-user',function() {
+    return view('after_login.add-user');
+});
+
 Route::get('/contacts',function() {
     return view('after_login.contacts');
 });
@@ -28,13 +32,6 @@ Route::get('/contacts',function() {
 Route::get('/manager',function() {
     return view('after_login.manager');
 });
-<<<<<<< HEAD
-
-Route::get('/form',function() {
-    return view('after_login.form');
-});
-=======
 Route::post('/create-church','ChurchesController@create');
 Route::post('/create-user','ChurchesController@create_church_user');
 Route::get('/createchurches',function(){return view('after_login.create-church');});
->>>>>>> 6ff3206b8fbcb2faa7567a3e18426887b492c2e2
