5f3d4ad01b66f4afbb627292bedd1ada0239ea89
ociba <ocibajames@gmail.com>
2019-10-14 14:14:16 +0300

    search,save category

--- app/category.php
+++ app/category.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class category extends Model
 {
     //
-    protected $table ="categories"
+    protected $table ="category";
     protected $fillable = ['id','message_id','user_id','church_id','category'];
 }
